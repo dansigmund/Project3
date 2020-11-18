84c57d28007322686458639a9565948b6c64adc0
jmayrides <72412490+jmayrides@users.noreply.github.com>
2020-11-17 22:21:51 -0500

    Add files via upload

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,5 +1,13 @@
   <header class="mdl-layout__header">
     <div class="mdl-layout__header-row">
-     <h1 class="mdl-layout-title">2016 ELECTION RESULTS</h1>
+      <h1 class="mdl-layout-title">2016 ELECTION RESULTS</h1>
+
+      <div class="mdl-layout-spacer"></div>
+
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--align-right">
+
+        <h1 class="mdl-layout-title"><?php echo "<img src='images/clinton.png' style='width:50px;height:50px'>" ?>Clinton 227 : Trump 304<?php echo "<img src='images/trump.png' style='width:50px;height:50px'>" ?></h1>
+
+      </div>
     </div>
   </header>
