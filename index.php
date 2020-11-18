d93adecf27c3e0f1ca99c444f7813d8d65b20024
jmayrides <72412490+jmayrides@users.noreply.github.com>
2020-11-17 20:34:31 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,161 +1,144 @@
-<?php include 'includes/book-utilities.inc.php';
-$customers = readCustomers('data/customers.txt');
+<?php include 'includes/state-utilities.inc.php';
+$states = readStates('data/states.txt');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title>CRM</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue_grey-orange.min.css">
-<link rel="stylesheet" href="css/styles.css">
-<script src="https://code.jquery.com/jquery-1.7.2.min.js"></script>
-<script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
-<script src="js/jquery.sparkline.2.1.2.js"></script>
-<script type="text/javascript">
-$(function() {
-/** This code runs when everything has been loaded on the page */
-/* Inline sparklines take their values from the contents of the tag */
-  $('.inlinesparkline').sparkline('html', {
-  type: 'bar',
-  barColor: '#6200EA'
-  });
-});
-</script>
+
+  <title>Election 2016</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-red.min.css" />
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="https://code.jquery.com/jquery-1.7.2.min.js"></script>
+  <script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+  <script src="js/jquery.sparkline.2.1.2.js"></script>
+  <script type="text/javascript"></script>
+
 </head>
+
 <body>
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
 
-<?php include 'includes/header.inc.php'; ?>
-<?php include 'includes/left-nav.inc.php'; ?>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <?php include 'includes/header.inc.php'; ?>
 
-<main class="mdl-layout__content mdl-color--grey-50">
-<section class="page-content">
-<div class="mdl-grid">
-<!-- mdl-cell + mdl-card -->
-<div class="mdl-cell mdl-cell--7-col card-lesson mdl-card  mdl-shadow--2dp">
-<div class="mdl-card__title mdl-color--orange">
-<h2 class="mdl-card__title-text">Customers</h2>
-</div>
-<div class="mdl-card__supporting-text">
-<table class="mdl-data-table  mdl-shadow--2dp">
-  <thead>
-  <tr>
-    <th class="mdl-data-table__cell--non-numeric">Name</th>
-    <th class="mdl-data-table__cell--non-numeric">University</th>
-    <th class="mdl-data-table__cell--non-numeric">City</th>
-    <th>Sales</th>
-  </tr>
-  </thead>
-<tbody>
-<?php
-/******************* TO DO ***************************/
-/*Table displays only the first customer found in customers.txt
-AND the html is hard coded. Developer mentioned she needs to update 
-it to use a foreach loop to create a <tr> and <td> for each customer
-I would suggest printing out the contents of the $customers array
-that contains all of the customer data found in data.txt.
-foreach ($customers as $cust) {
-foreach($cust as $key=>$val) {
-  echo "key is $key and val is $val  <br>";
-}
-}
-Once you know what's in the array place it in the table.
-See the order table below for some more hints.
-*/
+    <main class="mdl-layout__content mdl-color--blue-50">
+      <section class="page-content">
+        <div class="mdl-grid">
+          <div class="mdl-cell mdl-cell--6-col card-lesson mdl-card  mdl-shadow--2dp">
+            <div class="mdl-card__title mdl-color--red mdl-color-text--white">
+              <h2 class="mdl-card__title-text">States</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+              <table class="mdl-data-table  mdl-shadow--2dp">
+                <thead>
+                  <tr>
+                    <th class="mdl-data-table__cell--non-numeric">State</th>
+                    <th class="mdl-data-table__cell--non-numeric">Capitol</th>
+                    <th class="mdl-data-table__cell--non-numeric">Population</th>
+                    <th class="mdl-data-table__cell--non-numeric">Electoral Votes</th>
+                  </tr>
+                </thead>
+                <tbody>
 
-  foreach($customers as $cust)  {      
-       echo '<tr>';
-       echo '<td class="mdl-data-table__cell--non-numeric"><a href="index.php?customer=' . $cust['id'] .'">' . $cust['name'] . '</a></td>';
-       echo '<td class="mdl-data-table__cell--non-numeric">' . $cust['university'] . '</td>';
-       echo '<td class="mdl-data-table__cell--non-numeric">' . $cust['city'] . '</td>';
-       echo '<td><span class="inlinesparkline">' . $cust['sales'] . '</span></td>';
-       echo '</tr>';
-    }
-?>
-</tbody>
-</table>
-</div>
-</div> <!-- / mdl-cell + mdl-card -->
-<div class="mdl-grid mdl-cell--5-col">
-<?php
-/**********************TO DO ************************* */
-/*See the URL that is created above for when the customer name is clicked.
-It contains a query string like customer=ID
-The code below checks if the request method is GET and if so it then
-checks for the customer value set in the query string of the URL */
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  if (isset($_GET['customer'])) {
-    $requestedCustomer = $customers[$_GET['customer']]; 
-?>
-<!-- mdl-cell + mdl-card -->
-<div class="mdl-cell mdl-cell--12-col card-lesson mdl-card  mdl-shadow--2dp">
-<div class="mdl-card__title mdl-color--deep-purple mdl-color-text--white">
-<h2 class="mdl-card__title-text">Customer Details</h2>
-</div>
-<div class="mdl-card__supporting-text">
+                  <?php
 
-<!--Add the customer information here-->
-<!--Again the customer information is hard coded. You must change
-this so that the customer that is clicked is the customer details
-that show below -->
-<h4><?php echo $requestedCustomer['name']?></h4>
-<?php echo $requestedCustomer['university']?><br>
-<?php echo $requestedCustomer['address']?><br>
-<?php echo $requestedCustomer['city'] . "," . $requestedCustomer['country']?><br>
+                  foreach ($states as $state) {
 
+                    $id = $state['id'];
+                    $url = "index.php?state=$id";
+                    $name = $state['name'];
+                    $capitol = $state['capitol'];
+                    $population = $state['population'];
+                    $numVotes = $state['numVotes'];
 
+                    echo '<tr>';
+                    echo "<td class='mdl-data-table__cell--non-numeric'><a href=$url>$name</a></td>";
+                    echo "<td class='mdl-data-table__cell--non-numeric'>$capitol</td>";
+                    echo "<td class='mdl-data-table__cell--non-numeric'>$population</td>";
+                    echo "<td class='mdl-data-table__cell--non-numeric'>$numVotes</td>";
+                    echo '</tr>';
+                  }
 
-</div>
-</div> <!-- / mdl-cell + mdl-card -->
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="mdl-grid mdl-cell--5-col">
+            <?php
 
-<!-- mdl-cell + mdl-card -->
-<div class="mdl-cell mdl-cell--12-col card-lesson mdl-card  mdl-shadow--2dp">
-<div class="mdl-card__title mdl-color--deep-purple mdl-color-text--white">
-<h2 class="mdl-card__title-text">Order Details</h2>
-</div>
-<div class="mdl-card__supporting-text">
-<?php
-/*This code creates the order table
-This code is complete and does not need to be modified */
-    $orders = readOrders($_GET['customer'], 'data/orders.txt');
-    if (is_null($orders)) {
-      echo 'No orders for ' . $requestedCustomer['name'];
-    } else {
-      ?>
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+              if (isset($_GET['state'])) {
+                $requestedState = $states[$_GET['state']];
+                $name = $requestedState['name'];
+                $capitol = $requestedState['capitol'];
+                $population = $requestedState['population'];
+                $numVotes = $requestedState['numVotes'];
+            ?>
+                <div class="mdl-cell mdl-cell--12-col card-lesson mdl-card  mdl-shadow--2dp">
+                  <div class="mdl-card__title mdl-color--red mdl-color-text--white">
+                    <h2 class="mdl-card__title-text">State Details</h2>
+                  </div>
+                  <div class="mdl-card__supporting-text">
 
-  <table class="mdl-data-table  mdl-shadow--2dp">
-    <thead>
-      <tr>
-        <th class="mdl-data-table__cell--non-numeric">Cover</th>
-        <th class="mdl-data-table__cell--non-numeric">ISBN</th>
-        <th class="mdl-data-table__cell--non-numeric">Title</th>
-      </tr>
-    </thead>
-    <tbody>
+                    <h4><?php echo "$name" ?></h4>
+                    <?php echo "Capitol: $capitol" ?><br>
+                    <?php echo "Population: $population" ?><br>
+                    <?php echo "Electoral College Votes: $numVotes" ?>
 
-      <?php foreach ($orders as $ord) {
-              echo '<tr>';
-              echo '<td><img src="images/tinysquare/' . $ord['isbn'] . '.jpg"></td>';
-              echo '<td>' . $ord['isbn'] . '</td>';
-              echo '<td class="mdl-data-table__cell--non-numeric">' . $ord['isbn'] . $ord['title'] . '</td>';
-              echo '</tr>    ';
+                  </div>
+                </div>
+
+                <div class="mdl-cell mdl-cell--12-col card-lesson mdl-card  mdl-shadow--2dp">
+                  <div class="mdl-card__title mdl-color--red mdl-color-text--white">
+                    <h2 class="mdl-card__title-text">State Map</h2>
+                  </div>
+                  <div class="mdl-card__supporting-text">
+                    <?php
+
+                    $orders = readOrders($_GET['customer'], 'data/orders.txt');
+                    if (is_null($orders)) {
+                      echo 'No orders for ' . $requestedCustomer['name'];
+                    } else {
+                    ?>
+
+                      <table class="mdl-data-table  mdl-shadow--2dp">
+                        <thead>
+                          <tr>
+                            <th class="mdl-data-table__cell--non-numeric">Cover</th>
+                            <th class="mdl-data-table__cell--non-numeric">ISBN</th>
+                            <th class="mdl-data-table__cell--non-numeric">Title</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+
+                          <?php foreach ($orders as $ord) {
+                            echo '<tr>';
+                            echo '<td><img src="images/tinysquare/' . $ord['isbn'] . '.jpg"></td>';
+                            echo '<td>' . $ord['isbn'] . '</td>';
+                            echo '<td class="mdl-data-table__cell--non-numeric">' . $ord['isbn'] . $ord['title'] . '</td>';
+                            echo '</tr>    ';
+                          } ?>
+
+                        </tbody>
+                      </table>
+                    <?php  } ?>
+                  </div>
+                </div>
+            <?php
+              }
             } ?>
-
-    </tbody>
-  </table>
-<?php  } ?>
-</div>
-</div> <!-- / mdl-cell + mdl-card -->
-<?php
-}
-} ?>
-</div>
-</div> <!-- / mdl-grid -->
-</section>
-</main>
-</div> <!-- / mdl-layout -->
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
 </body>
+
 </html>
