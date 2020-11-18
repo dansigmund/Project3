ba8de9636e9c5df0acb38fab51d54c1e22fbc316
jmayrides <72412490+jmayrides@users.noreply.github.com>
2020-11-17 20:36:03 -0500

    Add files via upload

---
+++ includes/state-utilities.inc.php
@@ -0,0 +1,29 @@
+<?php
+function readStates($filename) {
+   // read the file into memory; if there is an error then stop processing
+   $arr = file($filename) or die('ERROR: Cannot find file');
+
+   // our data is semicolon-delimited
+   $delimiter = ';';
+
+   // loop through each line of the file
+   foreach ($arr as $line) {  
+      // returns an array of strings where each element in the array
+      // corresponds to each substring between the delimiters
+      $splitcontents = explode($delimiter, $line);
+      
+      $aState = array();
+      
+      $aState['id'] = $splitcontents[0];
+      $aState['name'] = utf8_encode($splitcontents[1]);
+      $aState['capitol'] = utf8_encode($splitcontents[2]);
+      $aState['population'] = utf8_encode($splitcontents[3]);
+      $aState['numVotes'] = utf8_encode($splitcontents[4]);
+
+      // add customer to array of customers
+      $states[$aState['id']] = $aState;
+   }
+   return $states;
+}
+
+?>
