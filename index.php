6f496f8217c9f9c07f2dc5621c521fadb77e7dfc
jmayrides <jmayride@villanova.edu>
2020-11-17 22:54:15 -0500

    Updated index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $states = readStates('data/states.txt');
 
 <head>
 
-  <title>Election 2016</title>
+  <title>2016 Election Results</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.gstatic.com">
